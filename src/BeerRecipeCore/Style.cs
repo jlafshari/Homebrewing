ac10e5b74e1787d4708f7c9c7ca219576d4f7a9a
Jahan Afshari <jlafshari@yahoo.com>
2013-08-24 17:44:40 -0700

    Add Style and StyleType classes.

---
+++ src/BeerRecipeCore/Style.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerRecipeCore
+{
+    public class Style : BeerXmlObject
+    {
+        public Style(string name, int version, string notes, string category, int categoryNumber, string styleLetter, string styleGuide, string type,
+            float ogMin, float ogMax, float fgMin, float fgMax, float ibuMin, float ibuMax, float colorMin, float colorMax, float carbMin, float carbMax,
+            float abvMin, float abvMax, string profile, string ingredients, string examples)
+            : base(name, version, notes)
+        {
+        }
+    }
+}
