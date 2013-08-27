2d734d41a96705ac2114eac9d83f6cd024b0e72f
Jahan Afshari <jlafshari@yahoo.com>
2013-08-26 23:37:14 -0700

    Add StyleClassification class.

---
+++ src/BeerRecipeCore/StyleClassification.cs
@@ -0,0 +1,24 @@
+﻿namespace BeerRecipeCore
+{
+    public class StyleClassification
+    {
+        public StyleClassification(string styleLetter, string styleGuide)
+        {
+            m_styleLetter = styleLetter;
+            m_styleGuide = styleGuide;
+        }
+
+        public string StyleLetter
+        {
+            get { return m_styleLetter; }
+        }
+
+        public string StyleGuide
+        {
+            get { return m_styleGuide; }
+        }
+
+        private string m_styleLetter;
+        private string m_styleGuide;
+    }
+}
