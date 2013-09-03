579331147cd2a63743550dbad74ffb45dc36ed96
Jahan Afshari <jlafshari@yahoo.com>
2013-09-03 14:44:30 -0700

    Add BeerRecipeCore.Tests project and BitternessUtilityTests class.

---
+++ tests/BeerRecipeCore.Tests/BitternessUtilityTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BeerRecipeCore.Tests
+{
+    [TestClass]
+    public class BitternessUtilityTests
+    {
+        [TestMethod]
+        public void GetBitternessTest()
+        {
+        }
+    }
+}
