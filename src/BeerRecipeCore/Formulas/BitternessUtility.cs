ff1070ee36a5fa197a294a1a33175c047969c52d
Jahan Afshari <jlafshari@yahoo.com>
2013-09-03 21:37:01 -0700

    Get the bitterness in IBU units of a recipe.

--- src/BeerRecipeCore/Formulas/BitternessUtility.cs
+++ src/BeerRecipeCore/Formulas/BitternessUtility.cs
@@ -1,6 +1,38 @@
-﻿namespace BeerRecipeCore.Formulas
+﻿using System;
+using System.Collections.Generic;
+
+namespace BeerRecipeCore.Formulas
 {
     public static class BitternessUtility
     {
+        /// <summary>
+        /// Gets the bitterness of a recipe in IBU units.
+        /// </summary>
+        /// <param name="hops">The hops used in the recipe.</param>
+        /// <param name="recipeVolumeInGallons">The size of the recipe in gallons.</param>
+        /// <param name="boilGravity">The specific gravity of the recipe before boiling.</param>
+        public static int GetBitterness(IEnumerable<HopsIngredient> hops, float recipeVolumeInGallons, float boilGravity)
+        {
+            float totalBitterness = 0;
+
+            foreach (HopsIngredient hopIngredient in hops)
+            {
+                float hopUtilization = GetHopUtilization((int) hopIngredient.Time, boilGravity);
+
+                float alphaAcid = hopIngredient.HopsInfo.Characteristics.AlphaAcid;
+                totalBitterness += hopUtilization * alphaAcid * hopIngredient.Amount * c_ibuEnglishUnitsConstant / recipeVolumeInGallons;
+            }
+
+            return (int) Math.Round((double) totalBitterness);
+        }
+
+        private static float GetHopUtilization(int boilTimeInMinutes, float boilGravity)
+        {
+            float bignessFactor = 1.65f * (float) Math.Pow(0.000125, boilGravity - 1);
+            float boilTimeFactor = (1 - (float) Math.Pow(Math.E, -0.04 * boilTimeInMinutes)) / 4.15f;
+            return bignessFactor * boilTimeFactor;
+        }
+
+        const float c_ibuEnglishUnitsConstant = 74.89f;
     }
 }
--- tests/BeerRecipeCore.Tests/BitternessUtilityTests.cs
+++ tests/BeerRecipeCore.Tests/BitternessUtilityTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using BeerRecipeCore.Formulas;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BeerRecipeCore.Tests
@@ -9,6 +10,15 @@ namespace BeerRecipeCore.Tests
         [TestMethod]
         public void GetBitternessTest()
         {
+            Hops fuggles = new Hops("Fuggles", new HopsCharacteristics(4.50f, 2.00f), "Boil", "Test Notes", "UK");
+            HopsIngredient fugglesInRecipe = new HopsIngredient(fuggles) { Amount = 1f, Form = HopsForm.Leaf, FlavorType = HopsFlavorType.Bittering, Time = 60f };
+            
+            Hops goldings = new Hops("Goldings", new HopsCharacteristics(5.00f, 3.50f), "Boil", "Test Notes", "UK");
+            HopsIngredient goldingsInRecipe = new HopsIngredient(goldings) { Amount = 1f, Form = HopsForm.Pellet, FlavorType = HopsFlavorType.Aroma, Time = 15f };
+
+            List<HopsIngredient> hopsUsed = new List<HopsIngredient>() { fugglesInRecipe, goldingsInRecipe };
+            int bitterness = BitternessUtility.GetBitterness(hopsUsed, 5f, 1.054f);
+            Assert.AreEqual(23, bitterness);
         }
     }
 }
