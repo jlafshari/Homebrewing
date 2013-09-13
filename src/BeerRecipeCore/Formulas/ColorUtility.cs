e52d7c31c8fa1716b97a8545e3047e70deb9da90
Jahan Afshari <jlafshari@yahoo.com>
2013-09-12 20:14:14 -0700

    Implement calculation of color in SRM units.

--- src/BeerRecipeCore/Formulas/ColorUtility.cs
+++ src/BeerRecipeCore/Formulas/ColorUtility.cs
@@ -1,6 +1,28 @@
-﻿namespace BeerRecipeCore.Formulas
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BeerRecipeCore.Formulas
 {
     public static class ColorUtility
     {
+        public static float GetColorInSrm(IEnumerable<FermentableIngredient> fermentables, float recipeVolumeInGallons)
+        {
+            // calculate MCU from each fermentable
+            float totalMcu = 0;
+            foreach (FermentableIngredient fermentable in fermentables)
+            {
+                float fermentableMcu = GetMaltColorUnit(fermentable, recipeVolumeInGallons);
+                totalMcu += fermentableMcu;
+            }
+            
+            // get SRM color using Morey equation
+            return 1.4922f * (float) Math.Pow(totalMcu, 0.6859);
+        }
+
+        private static float GetMaltColorUnit(FermentableIngredient fermentable, float recipeVolumeInGallons)
+        {
+            return fermentable.FermentableInfo.Characteristics.Color * fermentable.Amount / recipeVolumeInGallons;
+        }
     }
 }
---
+++ tests/BeerRecipeCore.Tests/ColorUtilityTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using BeerRecipeCore.Formulas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BeerRecipeCore.Tests
+{
+    [TestClass]
+    public class ColorUtilityTests
+    {
+        [TestMethod]
+        public void GetColorInSrmTest()
+        {
+            Fermentable crystal60 = new Fermentable("Caramel/Crystal Malt - 60L", new FermentableCharacteristics(74, 60, 0), "Test Notes", "US");
+            FermentableIngredient crystal60InRecipe = new FermentableIngredient(crystal60) { Amount = 0.50f, Type = FermentableType.Grain };
+
+            double crystal60Color = Math.Round(ColorUtility.GetColorInSrm(new[] { crystal60InRecipe }, 5), 1);
+            Assert.AreEqual(5.1, crystal60Color);
+
+            Fermentable marisOtter = new Fermentable("Pale Malt, Maris Otter", new FermentableCharacteristics(82.5f, 3, 120), "Test Notes", "US");
+            FermentableIngredient marisOtterInRecipe = new FermentableIngredient(marisOtter) { Amount = 8, Type = FermentableType.Grain };
+
+            List<FermentableIngredient> fermentablesUsed = new List<FermentableIngredient>() { marisOtterInRecipe, crystal60InRecipe };
+            double colorInSrm = Math.Round(ColorUtility.GetColorInSrm(fermentablesUsed, 5), 1);
+            Assert.AreEqual(7.6, colorInSrm);
+        }
+    }
+}
