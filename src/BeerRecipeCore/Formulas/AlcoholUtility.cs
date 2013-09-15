a739d90e84f1db87d9d4a4e77ba9d7d9be09b880
Jahan Afshari <jlafshari@yahoo.com>
2013-09-14 23:09:24 -0700

    Add methods to calculate alcohol by volume and by weight.

--- src/BeerRecipeCore/Formulas/AlcoholUtility.cs
+++ src/BeerRecipeCore/Formulas/AlcoholUtility.cs
@@ -2,5 +2,14 @@
 {
     public static class AlcoholUtility
     {
+        public static float GetAlcoholByVolume(float startingGravity, float finalGravity)
+        {
+            return (1.05f * (startingGravity - finalGravity)) / (finalGravity * 0.79f) * 100;
+        }
+
+        public static float GetAlcoholByWeight(float alcoholByVolume)
+        {
+            return alcoholByVolume * 0.79336f;
+        }
     }
 }
---
+++ tests/BeerRecipeCore.Tests/AlcoholUtilityTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using BeerRecipeCore.Formulas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BeerRecipeCore.Tests
+{
+    [TestClass]
+    public class AlcoholUtilityTests
+    {
+        [TestMethod]
+        public void GetAlcoholByVolumeTest()
+        {
+            float originalGravity = 1.045f;
+            float finalGravity = 1.006f;
+            float actualAbv = (float) Math.Round(AlcoholUtility.GetAlcoholByVolume(originalGravity, finalGravity), 2);
+            Assert.AreEqual(5.15f, actualAbv);
+        }
+
+        [TestMethod]
+        public void GetAlcoholByWeightTest()
+        {
+            float actualAbw = (float) Math.Round(AlcoholUtility.GetAlcoholByWeight(5.12f), 2);
+            Assert.AreEqual(4.06f, actualAbw);
+        }
+    }
+}
