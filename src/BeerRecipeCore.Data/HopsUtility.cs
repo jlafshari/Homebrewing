b4528ba4585e5b7bb4828b436504ec7902194be8
Jahan Afshari <jlafshari@yahoo.com>
2013-10-20 23:00:18 -0700

    Only select needed columns when getting hops records.

--- src/BeerRecipeCore.Data/HopsUtility.cs
+++ src/BeerRecipeCore.Data/HopsUtility.cs
@@ -9,19 +9,18 @@ namespace BeerRecipeCore.Data
         public static IEnumerable<Hops> GetAvailableHopsVarieties(SQLiteConnection connection)
         {
             SQLiteCommand selectHopsCommand = connection.CreateCommand();
-            //selectHopsCommand.CommandText = "SELECT (name, alpha, use, notes, beta, hsi, origin) FROM Hops";
-            selectHopsCommand.CommandText = "SELECT * FROM Hops";
+            selectHopsCommand.CommandText = "SELECT name, alpha, use, notes, beta, hsi, origin FROM Hops";
             using (SQLiteDataReader reader = selectHopsCommand.ExecuteReader())
             {
                 while (reader.Read())
                 {
-                    string name = reader[1].ToString();
-                    float alphaAcid = (float) Convert.ToDouble(reader[2]);
-                    string use = reader[3].ToString();
-                    string notes = reader[4].ToString();
-                    float betaAcid = (float) Convert.ToDouble(reader[5]);
-                    float hsi = (float) Convert.ToDouble(reader[6]);
-                    string origin = reader[7].ToString();
+                    string name = reader[0].ToString();
+                    float alphaAcid = (float) Convert.ToDouble(reader[1]);
+                    string use = reader[2].ToString();
+                    string notes = reader[3].ToString();
+                    float betaAcid = (float) Convert.ToDouble(reader[4]);
+                    float hsi = (float) Convert.ToDouble(reader[5]);
+                    string origin = reader[6].ToString();
 
                     HopsCharacteristics characteristics = new HopsCharacteristics(alphaAcid, betaAcid) { Hsi = hsi };
                     yield return new Hops(name, characteristics, use, notes, origin);
