37ddd213d34b7147296dd7ab9177d40309dedb61
Jahan Afshari <jlafshari@yahoo.com>
2013-11-23 19:08:25 -0800

    Add recipe class.

---
+++ src/BeerRecipeCore/Recipe.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.ObjectModel;
+
+namespace BeerRecipeCore
+{
+    public class Recipe
+    {
+        /// <summary>
+        /// The final amount of beer in gallons.
+        /// </summary>
+        public float Size
+        {
+            get { return m_size; }
+            set { m_size = value; }
+        }
+
+        /// <summary>
+        /// The boil time in minutes.
+        /// </summary>
+        public int BoilTime
+        {
+            get { return m_boilTime; }
+            set { m_boilTime = value; }
+        }
+
+        public Style Style
+        {
+            get { return m_style; }
+            set { m_style = value; }
+        }
+
+        public Collection<HopsIngredient> HopsIngredients
+        {
+            get { return m_hopsIngredients; }
+            private set { m_hopsIngredients = value; }
+        }
+
+        public Collection<FermentableIngredient> FermentableIngredients
+        {
+            get { return m_fermentableIngredients; }
+            private set { m_fermentableIngredients = value; }
+        }
+
+        public Yeast Yeast
+        {
+            get { return m_yeast; }
+            set { m_yeast = value; }
+        }
+
+        public MashProfile MashProfile
+        {
+            get { return m_mashProfile; }
+            set { m_mashProfile = value; }
+        }
+
+        float m_size;
+        int m_boilTime;
+        Style m_style;
+        Collection<HopsIngredient> m_hopsIngredients = new Collection<HopsIngredient>();
+        Collection<FermentableIngredient> m_fermentableIngredients = new Collection<FermentableIngredient>();
+        Yeast m_yeast;
+        MashProfile m_mashProfile;
+    }
+}
