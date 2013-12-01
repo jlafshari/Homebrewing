cbca4c031521e5419e3a8e760ec3b5f55287162e
Jahan Afshari <jlafshari@yahoo.com>
2013-11-30 21:34:36 -0800

    Update loading of fermentable data from the database.

--- src/BeerRecipeCore.Data/FermentableUtility.cs
+++ src/BeerRecipeCore.Data/FermentableUtility.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
+using Utility;
 
 namespace BeerRecipeCore.Data
 {
@@ -9,20 +10,26 @@ namespace BeerRecipeCore.Data
         public static IEnumerable<Fermentable> GetAvailableFermentables(SQLiteConnection connection)
         {
             SQLiteCommand selectFermentablesCommand = connection.CreateCommand();
-            selectFermentablesCommand.CommandText = "SELECT name, yield, color, origin, notes, diastaticPower FROM Fermentables";
+            selectFermentablesCommand.CommandText = "SELECT name, yield, yieldByWeight, color, origin, notes, diastaticPower, type, gravityPoint FROM Fermentables";
             using (SQLiteDataReader reader = selectFermentablesCommand.ExecuteReader())
             {
                 while (reader.Read())
                 {
                     string name = reader[0].ToString();
-                    float yield = (float) Convert.ToDouble(reader[1]);
-                    float color = (float) Convert.ToDouble(reader[2]);
-                    string origin = reader[3].ToString();
-                    string notes = reader[4].ToString();
-                    string diastaticPowerValue = reader[5].ToString();
+                    string yieldValue = reader[1].ToString();
+                    float? yield = !string.IsNullOrEmpty(yieldValue) ? (float?) Convert.ToDouble(yieldValue) : null;
+                    string yieldByWeightValue = reader[2].ToString();
+                    float? yieldByWeight = !string.IsNullOrEmpty(yieldByWeightValue) ? (float?) Convert.ToDouble(yieldByWeightValue) : null;
+                    float color = (float) Convert.ToDouble(reader[3]);
+                    string origin = reader[4].ToString();
+                    string notes = reader[5].ToString();
+                    string diastaticPowerValue = reader[6].ToString();
                     float? diastaticPower = !string.IsNullOrEmpty(diastaticPowerValue) ? (float?) Convert.ToDouble(diastaticPowerValue) : null;
+                    FermentableType type = (FermentableType) EnumConverter.Parse(typeof(FermentableType), reader[7].ToString());
+                    int gravityPoint = Convert.ToInt32(reader[8].ToString());
 
-                    FermentableCharacteristics characteristics = new FermentableCharacteristics(yield, color, diastaticPower);
+                    FermentableCharacteristics characteristics = new FermentableCharacteristics(yield, color, diastaticPower) {
+                        YieldByWeight = yieldByWeight, Type = type, GravityPoint = gravityPoint };
                     yield return new Fermentable(name, characteristics, notes, origin);
                 }
             }
