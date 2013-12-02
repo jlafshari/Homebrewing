a0e4cd704c55f780165d5686df2858f63b9e82c4
Jahan Afshari <jlafshari@yahoo.com>
2013-12-01 23:40:27 -0800

    Add method to calculate expected original gravity for a recipe.

--- src/BeerRecipeCore/Formulas/AlcoholUtility.cs
+++ src/BeerRecipeCore/Formulas/AlcoholUtility.cs
@@ -1,15 +1,51 @@
-﻿namespace BeerRecipeCore.Formulas
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BeerRecipeCore.Formulas
 {
     public static class AlcoholUtility
     {
+        /// <summary>
+        /// Gets the percentage of alcohol by volume (ABV) produced between two specific gravity measurements.
+        /// </summary>
         public static float GetAlcoholByVolume(float startingGravity, float finalGravity)
         {
             return (1.05f * (startingGravity - finalGravity)) / (finalGravity * 0.79f) * 100;
         }
 
+        /// <summary>
+        /// Gets the percentage of alcohol by weight (ABW) from a given ABV value.
+        /// </summary>
         public static float GetAlcoholByWeight(float alcoholByVolume)
         {
             return alcoholByVolume * 0.79336f;
         }
+
+        /// <summary>
+        /// Gets the estimated original gravity using a default extraction efficiency.
+        /// </summary>
+        /// <param name="fermentableIngredients">The fermentable ingredients in the recipe.</param>
+        /// <param name="recipeSize">The recipe size in gallons.</param>
+        public static float GetOriginalGravity(IEnumerable<FermentableIngredient> fermentableIngredients, float recipeSize)
+        {
+            return GetOriginalGravity(fermentableIngredients, recipeSize, c_defaultExtractionEfficiency);
+        }
+
+        /// <summary>
+        /// Gets the estimated original gravity.
+        /// </summary>
+        /// <param name="fermentableIngredients">The fermentable ingredients in the recipe.</param>
+        /// <param name="recipeSize">The recipe size in gallons.</param>
+        /// <param name="extractionEfficiency">The expected extraction efficiency percentage.</param>
+        public static float GetOriginalGravity(IEnumerable<FermentableIngredient> fermentableIngredients, float recipeSize, float extractionEfficiency)
+        {
+            float gravityPoints = fermentableIngredients.Select(ingredient => ingredient.Amount * ingredient.FermentableInfo.Characteristics.GravityPoint).Sum();
+            float pointsPerPound = (gravityPoints / recipeSize) * (extractionEfficiency / 100f);
+            pointsPerPound = (float) Math.Round((double) pointsPerPound);
+            return 1f + (pointsPerPound / 1000f);
+        }
+
+        const float c_defaultExtractionEfficiency = 60f;
     }
 }
--- tests/BeerRecipeCore.Tests/AlcoholUtilityTests.cs
+++ tests/BeerRecipeCore.Tests/AlcoholUtilityTests.cs
@@ -23,5 +23,20 @@ namespace BeerRecipeCore.Tests
             float actualAbw = (float) Math.Round(AlcoholUtility.GetAlcoholByWeight(5.12f), 2);
             Assert.AreEqual(4.06f, actualAbw);
         }
+
+        [TestMethod]
+        public void GetSpecificGravityTest()
+        {
+            Fermentable crystal60 = new Fermentable("Caramel/Crystal Malt - 60L", new FermentableCharacteristics(74, 60, 0) { Type = FermentableType.Grain, GravityPoint = 34 }, "Test Notes", "US");
+            FermentableIngredient crystal60InRecipe = new FermentableIngredient(crystal60) { Amount = 0.50f };
+            Fermentable chocolateMalt = new Fermentable("Chocolate Malt", new FermentableCharacteristics(60, 350, 0) { Type = FermentableType.Grain, GravityPoint = 28 }, "Test Notes", "US");
+            FermentableIngredient chocolateMaltInRecipe = new FermentableIngredient(chocolateMalt) { Amount = 1 };
+            Fermentable marisOtter = new Fermentable("Pale Malt, Maris Otter", new FermentableCharacteristics(82.5f, 3, 120) { Type = FermentableType.Grain, GravityPoint = 38 }, "Test Notes", "US");
+            FermentableIngredient marisOtterInRecipe = new FermentableIngredient(marisOtter) { Amount = 8 };
+
+            List<FermentableIngredient> fermentablesInRecipe = new List<FermentableIngredient>() { crystal60InRecipe, chocolateMaltInRecipe, marisOtterInRecipe };
+            float actualSpecificGravity = AlcoholUtility.GetOriginalGravity(fermentablesInRecipe, 5, 70);
+            Assert.AreEqual(1.049f, actualSpecificGravity);
+        }
     }
 }
