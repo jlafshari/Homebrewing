4f40c9148e9ce6d12fd41ffb263d92b8019c07f1
Jahan Afshari <jlafshari@yahoo.com>
2013-12-02 23:08:29 -0800

    Add beer batch class.

---
+++ src/BeerRecipeCore/Batch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace BeerRecipeCore
+{
+    public class Batch
+    {
+        public Batch(string brewerName, Recipe recipe)
+        {
+            m_brewerName = brewerName;
+            m_recipe = recipe;
+        }
+
+        public string BrewerName
+        {
+            get { return m_brewerName; }
+            set { m_brewerName = value; }
+        }
+
+        public string AssistantBrewerName
+        {
+            get { return m_assistantBrewerName; }
+            set { m_assistantBrewerName = value; }
+        }
+
+        public DateTime BrewingDate
+        {
+            get { return m_brewingDate; }
+            set { m_brewingDate = value; }
+        }
+
+        public Recipe Recipe
+        {
+            get { return m_recipe; }
+            set { m_recipe = value; }
+        }
+
+        public GravityReading OriginalGravity
+        {
+            get { return m_originalGravity; }
+            set { m_originalGravity = value; }
+        }
+
+        public GravityReading FinalGravity
+        {
+            get { return m_finalGravity; }
+            set { m_finalGravity = value; }
+        }
+
+        private string m_brewerName;
+        private string m_assistantBrewerName;
+        private DateTime m_brewingDate;
+        private Recipe m_recipe;
+        private GravityReading m_originalGravity;
+        private GravityReading m_finalGravity;
+    }
+}
---
+++ src/BeerRecipeCore/GravityReading.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BeerRecipeCore
+{
+    public class GravityReading
+    {
+        public GravityReading(double value, DateTime date)
+        {
+            m_value = value;
+            m_date = date;
+        }
+
+        public double Value
+        {
+            get { return m_value; }
+            set { m_value = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return m_date; }
+            set { m_date = value; }
+        }
+
+        private double m_value;
+        private DateTime m_date;
+    }
+}
