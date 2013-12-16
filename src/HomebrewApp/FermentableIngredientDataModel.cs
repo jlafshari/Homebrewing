74a417b12715f5de9ee1b0c7e3d8ffdee6e385e1
Jahan Afshari <jlafshari@yahoo.com>
2013-12-15 21:49:04 -0800

    Add FermentableIngredientDataModel class.

---
+++ src/HomebrewApp/FermentableIngredientDataModel.cs
@@ -0,0 +1,31 @@
+﻿using BeerRecipeCore;
+using MvvmFoundation.Wpf;
+
+namespace HomebrewApp
+{
+    public class FermentableIngredientDataModel : ObservableObject, IFermentableIngredient
+    {
+        public FermentableIngredientDataModel(Fermentable fermentableInfo)
+        {
+            m_fermentableInfo = fermentableInfo;
+        }
+
+        public float Amount
+        {
+            get { return m_amount; }
+            set
+            {
+                m_amount = value;
+                RaisePropertyChanged("Amount");
+            }
+        }
+
+        public Fermentable FermentableInfo
+        {
+            get { return m_fermentableInfo; }
+        }
+
+        private float m_amount;
+        private Fermentable m_fermentableInfo;
+    }
+}
