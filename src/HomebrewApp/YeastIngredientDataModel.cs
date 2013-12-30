91abc71508a2fba99f8c5ece7f572870c0e76610
Jahan Afshari <jlafshari@yahoo.com>
2013-12-29 22:20:11 -0800

    Add YeastIngredientDataModel class.

---
+++ src/HomebrewApp/YeastIngredientDataModel.cs
@@ -0,0 +1,48 @@
+﻿using BeerRecipeCore;
+using MvvmFoundation.Wpf;
+
+namespace HomebrewApp
+{
+    public class YeastIngredientDataModel : ObservableObject, IYeastIngredient
+    {
+        public YeastIngredientDataModel(Yeast yeastInfo)
+        {
+            m_yeastInfo = yeastInfo;
+        }
+
+        public float Weight
+        {
+            get
+            {
+                return m_weight;
+            }
+            set
+            {
+                m_weight = value;
+                RaisePropertyChanged("Weight");
+            }
+        }
+
+        public float Volume
+        {
+            get
+            {
+                return m_volume;
+            }
+            set
+            {
+                m_volume = value;
+                RaisePropertyChanged("Volume");
+            }
+        }
+
+        public Yeast YeastInfo
+        {
+            get { return m_yeastInfo; }
+        }
+
+        private float m_weight;
+        private float m_volume;
+        private Yeast m_yeastInfo;
+    }
+}
