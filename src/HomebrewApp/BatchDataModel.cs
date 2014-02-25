4e5e6c06d409cdcc64ef97ceee9bf51907b7a6b4
Jahan Afshari <jlafshari@yahoo.com>
2014-02-24 22:51:22 -0800

    Add batch and recipe data model classes.

---
+++ src/HomebrewApp/BatchDataModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using BeerRecipeCore;
+using MvvmFoundation.Wpf;
+
+namespace HomebrewApp
+{
+    public class BatchDataModel : ObservableObject, IBatch
+    {
+        public string BrewerName
+        {
+            get { return m_brewerName; }
+            set
+            {
+                m_brewerName = value;
+                RaisePropertyChanged("BrewerName");
+            }
+        }
+
+        public string AssistantBrewerName
+        {
+            get { return m_assistantBrewerName; }
+            set
+            {
+                m_assistantBrewerName = value;
+                RaisePropertyChanged("AssistantBrewerName");
+            }
+        }
+
+        public DateTime BrewingDate
+        {
+            get { return m_brewingDate; }
+            set
+            {
+                m_brewingDate = value;
+                RaisePropertyChanged("BrewingDate");
+            }
+        }
+
+        public IRecipe Recipe
+        {
+            get { return m_recipe; }
+            set
+            {
+                m_recipe = value;
+                RaisePropertyChanged("Recipe");
+            }
+        }
+
+        public GravityReading OriginalGravity
+        {
+            get { return m_originalGravity; }
+            set
+            {
+                m_originalGravity = value;
+                RaisePropertyChanged("OriginalGravity");
+            }
+        }
+
+        public GravityReading FinalGravity
+        {
+            get { return m_finalGravity; }
+            set
+            {
+                m_finalGravity = value;
+                RaisePropertyChanged("FinalGravity");
+            }
+        }
+
+        public ObservableCollection<GravityReading> RecordedGravityReadings
+        {
+            get { return m_recordedGravityReadings; }
+            private set
+            {
+                m_recordedGravityReadings = value;
+                RaisePropertyChanged("RecordedGravityReadings");
+            }
+        }
+
+        string m_brewerName;
+        string m_assistantBrewerName;
+        DateTime m_brewingDate;
+        IRecipe m_recipe;
+        GravityReading m_originalGravity;
+        GravityReading m_finalGravity;
+        ObservableCollection<GravityReading> m_recordedGravityReadings = new ObservableCollection<GravityReading>();
+    }
+}
---
+++ src/HomebrewApp/RecipeDataModel.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.ObjectModel;
+using BeerRecipeCore;
+using MvvmFoundation.Wpf;
+
+namespace HomebrewApp
+{
+    public class RecipeDataModel : ObservableObject, IRecipe
+    {
+        public float Size
+        {
+            get { return m_size; }
+            set
+            {
+                m_size = value;
+                RaisePropertyChanged("Size");
+            }
+        }
+
+        public int BoilTime
+        {
+            get { return m_boilTime; }
+            set
+            {
+                m_boilTime = value;
+                RaisePropertyChanged("BoilTime");
+            }
+        }
+
+        public Style Style
+        {
+            get { return m_style; }
+            set
+            {
+                m_style = value;
+                RaisePropertyChanged("Style");
+            }
+        }
+
+        public ObservableCollection<IHopsIngredient> HopsIngredients
+        {
+            get { return m_hopsIngredients; }
+            private set
+            {
+                m_hopsIngredients = value;
+                RaisePropertyChanged("HopsIngredients");
+            }
+        }
+
+        public ObservableCollection<IFermentableIngredient> FermentableIngredients
+        {
+            get { return m_fermentableIngredients; }
+            private set
+            {
+                m_fermentableIngredients = value;
+                RaisePropertyChanged("FermentableIngredients");
+            }
+        }
+
+        public Yeast Yeast
+        {
+            get { return m_yeast; }
+            set
+            {
+                m_yeast = value;
+                RaisePropertyChanged("Yeast");
+            }
+        }
+
+        public MashProfile MashProfile
+        {
+            get { return m_mashProfile; }
+            set
+            {
+                m_mashProfile = value;
+                RaisePropertyChanged("MashProfile");
+            }
+        }
+
+        float m_size;
+        int m_boilTime;
+        Style m_style;
+        ObservableCollection<IHopsIngredient> m_hopsIngredients = new ObservableCollection<IHopsIngredient>();
+        ObservableCollection<IFermentableIngredient> m_fermentableIngredients = new ObservableCollection<IFermentableIngredient>();
+        Yeast m_yeast;
+        MashProfile m_mashProfile;
+    }
+}
