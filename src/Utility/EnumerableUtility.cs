fa7e3801606d8a960ec2dfda4dad9f8602a9783c
Jahan Afshari <jlafshari@yahoo.com>
2014-03-01 22:09:20 -0800

    Add EnumerableUtility class.

---
+++ src/Utility/EnumerableUtility.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Utility
+{
+    /// <summary>
+    /// Provides extension methods for IEnumerable objects.
+    /// </summary>
+    public static class EnumerableUtility
+    {
+        public static ReadOnlyObservableCollection<T> ToReadOnlyObservableCollection<T>(this IEnumerable<T> sequence)
+        {
+            if (sequence == null)
+                return null;
+
+            return sequence as ReadOnlyObservableCollection<T> ?? new ReadOnlyObservableCollection<T>(new ObservableCollection<T>(sequence));
+        }
+    }
+}
