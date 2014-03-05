dc4dbdfd74f7ca6a01c2dc44f1cc4d8e0703e4dd
Jahan Afshari <jlafshari@yahoo.com>
2014-03-04 23:37:25 -0800

    Add ToReadOnly() extension method.

--- src/Utility/EnumerableUtility.cs
+++ src/Utility/EnumerableUtility.cs
@@ -15,5 +15,13 @@ namespace Utility
 
             return sequence as ReadOnlyObservableCollection<T> ?? new ReadOnlyObservableCollection<T>(new ObservableCollection<T>(sequence));
         }
+
+        public static ReadOnlyCollection<T> ToReadOnly<T>(this IEnumerable<T> sequence)
+        {
+            if (sequence == null)
+                return null;
+
+            return sequence as ReadOnlyCollection<T> ?? new ReadOnlyCollection<T>(sequence as List<T> ?? new List<T>(sequence));
+        }
     }
 }
