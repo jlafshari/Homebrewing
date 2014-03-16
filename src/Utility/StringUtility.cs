d1799d23e3afe3c3a5b9dfa9cf4c6677f02fb47a
Jahan Afshari <jlafshari@yahoo.com>
2014-03-15 19:45:28 -0700

    Add StringUtility class for useful extension methods.

---
+++ src/Utility/StringUtility.cs
@@ -0,0 +1,13 @@
+﻿namespace Utility
+{
+    public static class StringUtility
+    {
+        /// <summary>
+        /// Determines if the given string is null or empty.
+        /// </summary>
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return str == null || str.Length == 0;
+        }
+    }
+}
