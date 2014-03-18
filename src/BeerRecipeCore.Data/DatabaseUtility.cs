d045c8c2280acbbbc888152ccf2ea0f0ba77bbd8
Jahan Afshari <jlafshari@yahoo.com>
2014-03-18 00:18:32 -0700

    Add method to get last inserted database table row ID.

---
+++ src/BeerRecipeCore.Data/DatabaseUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace BeerRecipeCore.Data
+{
+    internal static class DatabaseUtility
+    {
+        internal static int GetLastInsertedRowId(SQLiteConnection connection)
+        {
+            using (SQLiteCommand getRowIdCommand = connection.CreateCommand())
+            {
+                getRowIdCommand.CommandText = "SELECT last_insert_rowid()";
+                return Convert.ToInt32(getRowIdCommand.ExecuteScalar());
+            }
+        }
+    }
+}
