a094675248318894a757c2d6cec17a967a2ecff8
Jahan Afshari <jlafshari@yahoo.com>
2014-05-07 23:00:02 -0700

    Add default recipe settings class.

---
+++ src/BeerRecipeCore/RecipeDefaultSettings.cs
@@ -0,0 +1,32 @@
+﻿namespace BeerRecipeCore
+{
+    /// <summary>
+    /// Default recipe settings based on a 5 gallon batch.
+    /// </summary>
+    public static class RecipeDefaultSettings
+    {
+        /// <summary>
+        /// The size in gallons.
+        /// </summary>
+        public static float Size
+        {
+            get { return 5f; }
+        }
+
+        /// <summary>
+        /// The boil time in minutes.
+        /// </summary>
+        public static int BoilTime
+        {
+            get { return 60; }
+        }
+
+        /// <summary>
+        /// The mash extraction efficiency percentage.
+        /// </summary>
+        public static float ExtractionEfficiency
+        {
+            get { return 60f; }
+        }
+    }
+}
