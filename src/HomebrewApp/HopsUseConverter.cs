52fe47d4cd3aded3973d88594b737dc3b54bbc83
Jahan Afshari <jlafshari@yahoo.com>
2014-05-30 23:47:54 -0700

    Add hop use and dry hop time columns to hops ingredients list view.

---
+++ src/HomebrewApp/HopsUseConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using BeerRecipeCore;
+using Utility;
+
+namespace HomebrewApp
+{
+    [ValueConversion(typeof(HopsUse), typeof(string))]
+    public class HopsUseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((HopsUse) value).SaveToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
