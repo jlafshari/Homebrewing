d6078f04c0f0be2b968b886be19d11b9b20cf92f
Jahan Afshari <jlafshari@yahoo.com>
2014-10-26 21:05:32 -0700

    Add gravity reading data model class.

---
+++ src/BeerRecipeCore.Data/Models/GravityReadingDataModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using MvvmFoundation.Wpf;
+
+namespace BeerRecipeCore.Data.Models
+{
+    public class GravityReadingDataModel : ObservableObject, IGravityReading
+    {
+        public GravityReadingDataModel(int gravityReadingId)
+        {
+            m_gravityReadingId = gravityReadingId;
+        }
+
+        public int GravityReadingId
+        {
+            get { return m_gravityReadingId; }
+        }
+
+        public double Value
+        {
+            get { return m_value; }
+            set
+            {
+                m_value = value;
+                RaisePropertyChanged("Value");
+            }
+        }
+
+        public DateTime Date
+        {
+            get { return m_date; }
+            set
+            {
+                m_date = value;
+                RaisePropertyChanged("Date");
+            }
+        }
+
+        int m_gravityReadingId;
+        double m_value;
+        DateTime m_date;
+    }
+}
