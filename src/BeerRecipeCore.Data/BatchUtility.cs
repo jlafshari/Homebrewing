997d21540e7da368173609d56c0dbf6b16cd12c7
Jahan Afshari <jlafshari@yahoo.com>
2014-12-28 23:34:36 -0800

    Add method to get all saved batches.

--- src/BeerRecipeCore.Data/BatchUtility.cs
+++ src/BeerRecipeCore.Data/BatchUtility.cs
@@ -1,11 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.SQLite;
+using System.Linq;
 using BeerRecipeCore.Data.Models;
 
 namespace BeerRecipeCore.Data
 {
     public static class BatchUtility
     {
+        public static IEnumerable<BatchDataModel> GetSavedBatches(IList<RecipeDataModel> availableRecipes)
+        {
+            using (SQLiteConnection connection = DatabaseUtility.GetNewConnection())
+            {
+                using (SQLiteCommand getBatchesCommand = connection.CreateCommand())
+                {
+                    getBatchesCommand.CommandText = "SELECT * FROM Batches";
+                    using (SQLiteDataReader reader = getBatchesCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int batchId = reader.GetInt32(0);
+                            int recipeId = reader.GetInt32(4);
+                            BatchDataModel batch = new BatchDataModel(batchId)
+                            {
+                                BrewerName = reader.GetString(1),
+                                AssistantBrewerName = reader.GetString(2),
+                                Recipe = availableRecipes.FirstOrDefault(recipe => recipe.RecipeId == recipeId)
+                            };
+
+                            DateTime brewingDate;
+                            if (DateTime.TryParse(reader.GetString(3), out brewingDate))
+                                batch.BrewingDate = brewingDate;
+
+                            // get gravity readings for batch
+                            using (SQLiteCommand getGravityReadingsForBatchCommand = connection.CreateCommand())
+                            {
+                                getGravityReadingsForBatchCommand.CommandText = "SELECT GravityReadings.id, GravityReadings.specificGravity, GravityReadings.date FROM GravityReadings " +
+                                    "JOIN GravityReadingsInBatch ON GravityReadingsInBatch.gravityReading = GravityReadings.id AND GravityReadingsInBatch.batch = @batchId";
+                                getGravityReadingsForBatchCommand.Parameters.AddWithValue("batchId", batchId);
+                                using (SQLiteDataReader gravityReadingsReader = getGravityReadingsForBatchCommand.ExecuteReader())
+                                {
+                                    while (gravityReadingsReader.Read())
+                                    {
+                                        GravityReadingDataModel gravityReading = new GravityReadingDataModel(gravityReadingsReader.GetInt32(0)) { Value = gravityReadingsReader.GetDouble(1) };
+                                        DateTime gravityReadingDate;
+                                        if (DateTime.TryParse(gravityReadingsReader.GetString(2), out gravityReadingDate))
+                                            gravityReading.Date = gravityReadingDate;
+
+                                        batch.RecordedGravityReadings.Add(gravityReading);
+                                    }
+                                }
+                            }
+
+                            yield return batch;
+                        }
+                    }
+                }
+                connection.Close();
+            }
+        }
+        
         public static void SaveBatch(BatchDataModel batch)
         {
             using (SQLiteConnection connection = DatabaseUtility.GetNewConnection())
