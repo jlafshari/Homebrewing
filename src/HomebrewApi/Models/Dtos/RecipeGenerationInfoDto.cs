2042a7aab6e32f7b32abec2f938f1d58168fbc7d
Jahan Afshari <afsharij@wwt.com>
2021-04-23 13:39:34 -0700

    convert DTO classes into record types

--- src/HomebrewApi/Models/Dtos/RecipeGenerationInfoDto.cs
+++ src/HomebrewApi/Models/Dtos/RecipeGenerationInfoDto.cs
@@ -1,11 +1,4 @@
 namespace HomebrewApi.Models.Dtos
 {
-    public class RecipeGenerationInfoDto
-    {
-        public float Size { get; init; }
-        public string StyleId { get; init; }
-        public float Abv { get; init; }
-        public int ColorSrm { get; init; }
-        public string Name { get; init; }
-    }
+    public record RecipeGenerationInfoDto(float Size, string StyleId, float Abv, int ColorSrm, string Name);
 }
