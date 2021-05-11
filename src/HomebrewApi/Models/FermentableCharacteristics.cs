9aae5474626a0efc91a1d7ec1ceda104479d94ba
Jahan Afshari <afsharij@wwt.com>
2021-05-10 17:33:02 -0700

    convert model classes into records

--- src/HomebrewApi/Models/FermentableCharacteristics.cs
+++ src/HomebrewApi/Models/FermentableCharacteristics.cs
@@ -2,29 +2,14 @@ using BeerRecipeCore.Fermentables;
 
 namespace HomebrewApi.Models
 {
-    public class FermentableCharacteristics
-    {
-        /// <summary>
-        /// The percent dry yield if the fermentable is a grain.
-        /// </summary>
-        public float? Yield { get; init; }
-
-        /// <summary>
-        /// The percent raw yield by weight if this is an extract, adjunct, or sugar.
-        /// </summary>
-        public float? YieldByWeight { get; init; }
-
-        public double Color { get; init; }
-
-        /// <summary>
-        /// The diastatic power in Lintner degree units. If the FermentableType is not a grain or adjunct, then this value is null.
-        /// </summary>
-        public float? DiastaticPower { get; init; }
-
-        public FermentableType Type { get; init; }
-
-        public MaltCategory? MaltCategory { get; set; }
-
-        public int GravityPoint { get; init; }
-    }
+    public record FermentableCharacteristics
+    (
+        float? Yield,
+        float? YieldByWeight,
+        double Color,
+        float? DiastaticPower,
+        FermentableType Type,
+        MaltCategory? MaltCategory,
+        int GravityPoint
+    );
 }
--- src/HomebrewApi/Models/YeastCharacteristics.cs
+++ src/HomebrewApi/Models/YeastCharacteristics.cs
@@ -2,13 +2,13 @@ using BeerRecipeCore.Yeast;
 
 namespace HomebrewApi.Models
 {
-    public class YeastCharacteristics
-    {
-        public YeastType Type { get; set; }
-        public Flocculation Flocculation { get; set; }
-        public YeastForm Form { get; set; }
-        public float MinTemperature { get; set; }
-        public float MaxTemperature { get; set; }
-        public float Attenuation { get; set; }
-    }
+    public record YeastCharacteristics
+    (
+        YeastType Type,
+        Flocculation Flocculation,
+        YeastForm Form,
+        float MinTemperature,
+        float MaxTemperature,
+        float Attenuation
+    );
 }
