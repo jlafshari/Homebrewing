261a5e93dae6595c1ce5a7aea906cdec3403b6e6
Jahan Afshari <afsharij@wwt.com>
2021-05-13 14:20:57 -0700

    remove BsonId from FermentableId property since it is a foreign key, not a primary key

--- src/HomebrewApi/Models/FermentableIngredient.cs
+++ src/HomebrewApi/Models/FermentableIngredient.cs
@@ -5,7 +5,6 @@ namespace HomebrewApi.Models
 {
     internal record FermentableIngredient(float Amount)
     {
-        [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string FermentableId { get; set; }
     }
