545377da044e46b156b74c5ea8bff8b64ad57a22
Jahan Afshari <afsharij@wwt.com>
2021-05-17 17:54:03 -0700

    add validation to generate recipe endpoint dto class

---
+++ src/BeerRecipeCore/Recipes/RecipeGenerationConstants.cs
@@ -0,0 +1,14 @@
+namespace BeerRecipeCore.Recipes
+{
+    public static class RecipeGenerationConstants
+    {
+        public const float MinimumSize = 1;
+        public const float MaximumSize = 100;
+        public const float MinimumAbv = 1;
+        public const float MaximumAbv = 20;
+        public const int MinimumColor = 1;
+        public const int MaximumColor = 30;
+        public const int MinimumIbu = 1;
+        public const int MaximumIbu = 120;
+    }
+}
--- src/HomebrewApi/Models/Dtos/RecipeGenerationInfoDto.cs
+++ src/HomebrewApi/Models/Dtos/RecipeGenerationInfoDto.cs
@@ -1,4 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using Constants = BeerRecipeCore.Recipes.RecipeGenerationConstants;
+
 namespace HomebrewApi.Models.Dtos
 {
-    public record RecipeGenerationInfoDto(float Size, string StyleId, float Abv, int ColorSrm, int Ibu, string Name);
+    public record RecipeGenerationInfoDto(
+        [Required][Range(Constants.MinimumSize, Constants.MaximumSize)]float Size,
+        [Required]string StyleId,
+        [Required][Range(Constants.MinimumAbv, Constants.MaximumAbv)]float Abv,
+        [Required][Range(Constants.MinimumColor, Constants.MaximumColor)]int ColorSrm,
+        [Required][Range(Constants.MinimumIbu, Constants.MaximumIbu)]int Ibu,
+        [Required]string Name);
 }
